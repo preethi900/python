1a9289f1b984b40c006e0d697f33afdec2731723
Preethi <preethi900@gmail.com>
2018-08-27 17:53:59 -0700

    date program

---
+++ date.py
@@ -0,0 +1,22 @@
+#date program
+
+from datetime import datetime
+
+now = datetime.now()
+
+print now
+
+mm = str(now.month)
+
+dd = str(now.day)
+
+yyyy = str(now.year)
+
+hour = str(now.hour)
+
+mi = str(now.minute)
+
+ss = str(now.second)
+
+print mm + "/" + dd + "/" + yyyy + " " + hour + ":" + mi + ":" + ss
+
