5a45534d663fb875c6a89f2a04688610f98dafa0
Preethi <preethi900@gmail.com>
2018-10-01 10:35:52 -0700

    detect capital

---
+++ detectcapital.py
@@ -0,0 +1,9 @@
+#detect capital
+
+class Solution(object):
+    def detectCapitalUse(self, word):
+        """
+        :type word: str
+        :rtype: bool
+        """
+        return word.islower() or word.isupper() or word.istitle()
